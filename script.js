1427ce80d0cc3374e234b5a3df66c18a99223a34
Rylan Reese <rylanreese@gmail.com>
2022-08-10 23:15:40 -0400

    Worked on string methods

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Hello World")
 // Dont need to specify type, compiler does it for you.
+// Function and Methods are the same thing.
 
 // string is anything written in quotes, a series of characters (specific letter).
 let string="string"
@@ -132,6 +133,9 @@ if(a>d){
     case 3:
         console.log("Line 120 is true:", a)
     break;
+    default:
+        console.log("Default:", )
+    break;
  }
  // A switch statment will run each block until a statement can be excuted, if none can be then it will run to the default block.
  const Football="Browns"
@@ -146,4 +150,48 @@ switch(Football){
     case "Blue Jackets":
         console.log("This is not a football team:", Football)
     break;
+    default:
+        console.log("Try a different team:", )
+    break;
+// boolean is true or false.
 }
+let isClicked=true
+if(isClicked==true){
+    isClicked=false
+    console.log(isClicked)
+} else{
+  isClicked=true
+  console.log(isClicked)  
+}
+// word.length gives you the length of the string variable. if(bird) creates a boolean statement, if there are characters in the string = true, none= false.
+var word="bananas"
+let bird=word.length
+if(bird){
+    console.log("string length:", bird) 
+} else{
+    console.log(word)
+}
+// /n new line. /t tab over. /v vertical tabular. 
+// .slice (x,y) is the start and end of your string. this example is (7,13), 7 over from beginning is your start & 13 is your end.
+// .slice (7) creates the start of the string 7 characters over from the original beginning.
+// .slice (-7) starts from the end of the string and goes over 7 characters, the last 7 characters of the original string.
+let Ipsum="Apple, Banana, Kiwi"
+// let temp=Ipsum.slice(-7)
+// console.log(temp,temp.length)
+// .substring(7,13) keeps the characters between those parameters.
+// .replace replaces the characters in the string with what you want. Has to be identical.
+let temp1=Ipsum.replace("Banana", "Strawberry")
+// console.log(temp1,temp1.length)
+// .concat adds another variable or element.
+// .toLowerCase make the string all lowercase. .toUpperCase makes the string all uppercase
+let temp2=temp1.concat(", ", word)
+let password=temp2.toLowerCase()
+console.log(password)
+// .trim trims the excess spaces in the string.
+let temp3=Ipsum.trim()
+console.log(temp3)
+// .toString converts the type to a string type.
+let number1=5
+console.log(number1)
+let text=number1.toString()
+console.log(text)
